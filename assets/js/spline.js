8ac3d7f50f8d9e0fb6babd72f593c60ad85fa8b8
Tanzil Zubair Bin Zaman <tanzil070304@gmail.com>
2023-02-11 02:56:23 +0600

    Progress commit with safari bug

--- assets/js/spline.js
+++ assets/js/spline.js
@@ -1,25 +1,24 @@
-import { Application } from 'https://unpkg.com/@splinetool/runtime@0.9.210/build/runtime.js';
+import { Application } from 'https://unpkg.com/@splinetool/runtime@0.9.211/build/runtime.js';
 
 var introInitialized = false;
 var app;
 function initializeIntro() {
-    // if (introInitialized == false) {
-    //     const canvas = document.getElementById('intro-3d');
-    //     app = new Application(canvas);
-    //     app.load('./assets/models/intro.splinecode').then(() => {
-    //         app.setZoom(0.9);
-    //         console.log(app.getSplineEvents());
-    //     });
+    if (introInitialized == false) {
+        const canvas = document.getElementById('intro-3d');
+        app = new Application(canvas);
+        app.load('./assets/models/intro.splinecode').then(() => {
+            app.setZoom(0.9);
+        });
 
-    //     introInitialized = true;
-    // }
+        introInitialized = true;
+    }
 }
 
 function destroyIntro() {
-    // if (introInitialized == true) {
-    //     app.dispose();
-    //     $("#intro-3d").replaceWith('<canvas id="intro-3d" ></canvas>');
-    //     introInitialized = false;
-    // }
+    if (introInitialized == true) {
+        app.dispose();
+        $("#intro-3d").replaceWith('<canvas id="intro-3d" ></canvas>');
+        introInitialized = false;
+    }
 }
 export { initializeIntro, destroyIntro };
